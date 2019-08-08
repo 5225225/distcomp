26fe905dead2d2e05de054962a05800e50aa34b7
5225225 <5225225@mailbox.org>
2019-08-08 20:45:40 +0100

    Initial work before refactoring

--- src/bin/distcomp.rs
+++ src/bin/distcomp.rs
@@ -2,6 +2,7 @@ use distcomp::data_structures::kvs::KeyValueStore;
 use distcomp::{ApplicationId, Journal, SqliteJournal};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
+use wasmi::{ImportsBuilder, ModuleInstance, NopExternals, RuntimeValue};
 
 #[derive(Serialize, Deserialize, Default, Debug, Hash, Clone)]
 struct Password {
@@ -39,13 +40,43 @@ fn do_foo(journal: &SqliteJournal, appid: ApplicationId, num: i32) {
     journal.update_state(&pwmandata, appid);
 }
 
+fn func_main() {
+    // Parse WAT (WebAssembly Text format) into wasm bytecode.
+    let wasm_binary: Vec<u8> = wabt::wat2wasm(
+        r#"
+            (module
+                (func (export "test") (result i32)
+                    i32.const 1337
+                )
+            )
+            "#,
+    )
+    .expect("failed to parse wat");
+
+    // Load wasm binary and prepare it for instantiation.
+    let module = wasmi::Module::from_buffer(&wasm_binary).expect("failed to load wasm");
+
+    // Instantiate a module with empty imports and
+    // assert that there is no `start` function.
+    let instance = ModuleInstance::new(&module, &ImportsBuilder::default())
+        .expect("failed to instantiate wasm module")
+        .assert_no_start();
+
+    // Finally, invoke the exported function "test" with no parameters
+    // and empty external function executor.
+    assert_eq!(
+        instance
+            .invoke_export("test", &[], &mut NopExternals,)
+            .expect("failed to execute export"),
+        Some(RuntimeValue::I32(1337)),
+    );
+}
+
 fn main() {
     better_panic::install();
     let journal = SqliteJournal::new("sqlite.db");
 
     let appid = ApplicationId(Uuid::parse_str("f524b42d-7108-4489-8c84-988462634d39").unwrap());
 
-    for i in 0..1000 {
-        do_foo(&journal, appid, dbg!(i));
-    }
+    func_main();
 }
